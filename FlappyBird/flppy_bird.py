5ae56ff32bfcf53e417a020c1ca10f7e2f7243c3
yashodeepchikte <yashodeepchikte@gmail.com>
2020-06-17 19:36:43 +0530

    Added the draw function that combines bird, pipes, base objects

--- FlappyBird/flppy_bird.py
+++ FlappyBird/flppy_bird.py
@@ -3,7 +3,7 @@ import os
 import random
 import time
 import neat
-
+pygame.font.init()
 MIN_WIDTH = 500
 MIN_HEIGHT = 750
 
@@ -21,6 +21,8 @@ BASE_IMG = pygame.transform.scale2x(
     pygame.image.load(os.path.join("images", "base.png")))
 BG_IMG = pygame.transform.scale2x(
     pygame.image.load(os.path.join("images", "bg.png")))
+
+STAT_FONT = pygame.font.SysFont("comicsans", 50)
 
 
 class Bird:
@@ -103,7 +105,7 @@ class Pipe:
         self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
         self.PIIPE_BOTTOM = PIPE_IMG
 
-        self.past = False
+        self.passed = False
         self.set_height()
 
     def set_height(self):
@@ -159,16 +161,28 @@ class Base:
         win.blit(self.IMG, (self.x2, self.y))
 
 
-def draw_window(win, bird):
+def draw_window(win, bird, pipes, base, score):
     win.blit(BG_IMG, (0, 0))
+    for pipe in pipes:
+        pipe.draw(win)
+
+    text = STAT_FONT.render("Score : " + str(score), 1, (255, 255, 255))
+    win.blit(text, (MIN_WIDTH - 10 - text.get_width(), 10))
+
+    base.draw(win)
     bird.draw(win)
+
     pygame.display.update()
 
 
 def main():
-    bird = Bird(200, 200)
+    bird = Bird(250, 350)
+    base = Base(680)
+    pipes = [Pipe(700)]
     win = pygame.display.set_mode((MIN_WIDTH, MIN_HEIGHT))
     clock = pygame.time.Clock()
+    add_pipe = False
+    score = 0
 
     run = True
     while run:
@@ -176,8 +190,33 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:   # pylint: disable=no-member
                 run = False
-        bird.move()
-        draw_window(win, bird)
+        # bird.move()
+        rem = []
+        for pipe in pipes:
+            pipe.move()
+            if pipe.collide(bird):
+                pass
+            if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+                rem.append(pipe)
+
+            if not pipe.passed and pipe.x < bird.x:
+                pipe.passed = True
+                add_pipe = True
+
+        if add_pipe:
+            score += 1
+            pipes.append(Pipe(600))
+            add_pipe = False
+
+        for r in rem:
+            pipes.remove(r)
+
+        # check for base collision
+        if bird.y + bird.img.get_height() > 680:
+            pass
+
+        base.move()
+        draw_window(win, bird, pipes, base, score)
 
     pygame.quit()  # pylint: disable=no-member
     quit()
